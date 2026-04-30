e6f0640b5101635249dcd09757b3514c8a95c4db
Seyfelislam DJILALI AYAD <sifoislam2005@gmail.com>
2026-04-30 22:54:36 +0200

    Add files via upload

--- includes/header.php
+++ includes/header.php
@@ -1,7 +1,9 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <link rel="stylesheet" href="assets/css/style.css">
+
+    <link rel="stylesheet" href="../assets/css/style.css">
     <title><?php if(isset($page)){ echo "$page";} ?></title>
+
 </head>
 <body>
